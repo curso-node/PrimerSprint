54e178d84d0c2ce06a607d7a46e75d36481d28e6
Andres <sernmandres@gmail.com>
2019-03-24 23:08:56 -0500

    Encabezado: cambio de express por config y cambio en funciones.js Decripcion: Se cambio el nombre del archivo para que sea más especifico a lo que hace y se quito las lineas dentro de funciones.js que no tenian ninguna relevancia

--- express.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static(__dirname + '/public'))
-app.listen(3000,
-	console.log("Ejecutandose en el puerto 3000 del localhost"))
--- public/script/funciones.js
+++
@@ -1,6 +0,0 @@
-alert("Dale click al texto");
-function cambiarColor() {
-	document.getElementById("title").innerHTML= "Welcome to our first Sprint :)";
-	document.getElementById("title").style.color="red";
-	
-}
