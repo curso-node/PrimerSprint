443f7bbca1f389607838331df9b97d244ce881b5
Edwiinrtz <edwiinrtz@gmail.com>
2019-03-31 15:09:36 -0500

    funciones admin-usu

--- funciones.js
+++ funciones.js
@@ -5,9 +5,10 @@ const añadirCurso=(data)=>{
 	const dataC ={
 	nombre:data.n,
 	id:data.i,
+	
 	duraccion: data.d,
 	costo:data.c,
-	estado:"offline",
+	estado:"disponible",
 	modalidad:data.m,
 	descripción:data.a
 	};	
@@ -16,6 +17,7 @@ const añadirCurso=(data)=>{
 	if (!Ident) {
 		let nm = cursos.find(varC => varC.nombre == dataC.nombre);
 		if (!nm) {
+
 			cursos.push(dataC);
 			guardar()
 
@@ -39,9 +41,124 @@ const cursosDisp=()=>{
 const guardar =()=>{
 	let texto = JSON.stringify(cursos)
 	fs.writeFile("listacursos.json",texto,(err)=> {
-		if(err){throw(err)}	else{console.log('creado')}});
+		if(err){throw(err)}	else{console.log('Realizado con exito ')}});
 }
 
+
+const regin=(datos)=>{ //registrar usuarios en cursos
+	cursosDisp()
+	UsuDisp()
+	regg()	
+	let data={
+		IdUsu:datos.Idusuario,
+		Idcur:datos.Idcurso
+	};
+	let curso = cursos.find(Idcurso => Idcurso.id == data.Idcur);
+	let Usu = listaUsu.find(Idusuario => Idusuario.id == data.IdUsu);
+	let hg = usuReg.find(vf => vf.usuarios.id ==Usu.id && vf.curso == curso.nombre)
+	if(!hg){
+		let br ={
+			curso:{
+				nombre:curso.nombre,
+				id:curso.id
+			},
+			usuarios:{
+				nombre: Usu.nombre,
+				id: Usu.id
+			}
+		}
+		usuReg.push(br)
+		guardarReg()
+		
+	}else{
+		console.log("ya hay un usuario con ese ID registrado en:"+curso.nombre)
+	}
+}
+const regg=()=>{ //llevar a usuReg los inscritos y sus cursos correspondientes
+	usuReg = require('./registrados')
+}
+const guardarReg =()=>{ //crear el archivo de registrados
+	let texto = JSON.stringify(usuReg)
+	fs.writeFile("registrados.json",texto,(err)=> {
+		if(err){throw(err)}	else{console.log('Realizado con exito')}});
+}
+const UsuDisp=()=>{
+		listaUsu = require('./usuarios')
+}
+
+
+const cursosOP=(opcion,lcurso)=>{
+	cursosDisp()
+	UsuDisp()
+	regg()
+	switch (opcion){
+		case "disponibles": //listar cursos disponibles
+			let Us = cursos.filter(ff=> ff.estado == "disponible")
+			for (var i = 0; i < Us.length; i++) {
+				console.log(Us[i])
+			}
+		break
+		case "inscritos": //listar inscritos
+			let ls = usuReg.filter(tt => tt.curso['nombre'] == lcurso)
+			for (var i = 0; i < ls.length; i++) {
+				console.log(ls[i].usuarios)	
+			}
+			console.log('Total inscritos:'+ls.length)
+		break
+		case "cerrar": //cerrar
+		let ab = cursos.find(abr => abr.nombre==lcurso)
+		if (!ab) {
+			console.log('Ese curso no existe')
+		}else{
+			ab['estado']="cerrado"
+			guardar()
+		}
+		break
+	}
+}
+const actualizar=(datos)=>{
+	UsuDisp()
+	let usua = listaUsu.find(wh => wh.id == datos.u)
+	if (!usua) {
+		console.log("El usuario no existe")
+	}else{
+		usua[datos.o] = datos.n
+		guardarUsu()
+
+	}
+}
+const guardarUsu=()=>{
+	let txt = JSON.stringify(listaUsu)
+	fs.writeFile("usuarios.json",txt,(err)=> {
+		if(err){throw(err)}	else{console.log('Realizado con exito')}})
+}
+const eliminar=(datos)=>{
+	cursosDisp()
+	UsuDisp()
+	regg()
+	let dataUs = listaUsu.find(xf => xf.id == datos.u)
+	let dataCur = cursos.find(xfc => xfc.id == datos.c)
+	if (!dataUs || !dataCur) {
+		console.log("Uno de los datos es invalido. ¡verifique!")
+	}else{
+
+		let nEliminar = usuReg.filter(nn => nn.curso.id != datos.c && nn.usuarios.id != datos.u)
+		console.log(nEliminar)
+		// usuReg=nEliminar
+		// guardarReg() 
+
+	}
+}
+
+
+
+
+
 module.exports={
-	añadirCurso
+	añadirCurso,
+	regin,
+	cursosOP,
+	actualizar,
+	eliminar
+
 }
--- yargs.js
+++ yargs.js
@@ -1,5 +1,5 @@
 const comandos={
-	opción:{
+	opcion:{
 		alias:"o",
 		demand:true
 	},
@@ -20,23 +20,89 @@ const comandos={
 		demand:true
 	},
 	duraccion:{
-		alias:"d",
-		demand:true
+		alias:"d"
 	},
 	modalidad:{
-		alias:"m",
+		alias:"m"
+	}
+}
+const comandosreginCu={
+	opcion:{
+		alias:"o",
+		demand:true
+	},
+	Idusuario:{
+		alias:"i",
+		demand:true
+	},
+	Idcurso:{
+		alias:"c",
 		demand:true
 	}
 }
-const funciones = require('./funciones')
-
-const argv = require("yargs").command("coordinador","coordinador del programa de educación continua",comandos).argv
-
-if (argv._ == "coordinador") {
-	switch (argv.o) {
-		case "new":
-			funciones.añadirCurso(argv);
-		break;
+const cursos={
+	opcion:{
+		alias:'o',
+		demand:true
+	},
+	curso:{
+		alias:"c"
+	}
+}
+const actualizar={
+	opcion:{
+		alias:"o",
+		demand:true
+	},
+	usuario:{
+		alias:"u",
+		demand:true
+	},
+	nuevo:{
+		alias:"n",
+		demand:true
+	}
+}
+const eliminar={
+	usuario:{
+		alias:"u",
+		demand:true
+	},
+	curso:{
+		alias:"c",
+		demand:true
 	}
 }
 
+const funciones = require('./funciones')
+
+const argv = require("yargs").command("coordinador","coordinador del programa de educación continua",comandos)
+							.command("usuario","registrar usuario en un curso",comandosreginCu)
+							.command("cursos","listar todos los cursos o por usuario",cursos)
+							.command("actualizar","actualizar datos usuarios",actualizar)
+							.command("eliminar","eliminar <x> usuario de <y> curso",eliminar)
+							.argv
+switch (argv._[0]){
+	case "coordinador":
+		switch (argv.o) {
+			case "new":
+				funciones.añadirCurso(argv);
+			break;
+		}
+	break;
+	case "usuario":
+		switch(argv.o){
+			case "reg":
+			funciones.regin(argv);
+		}
+	break;
+	case "cursos":
+		funciones.cursosOP(argv.o,argv.c)
+	break;
+	case "actualizar":
+		funciones.actualizar(argv)
+	break;
+	case "eliminar":
+		funciones.eliminar(argv)
+	break
+}
